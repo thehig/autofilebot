fac90ddd3c24770dc539efb0ca1e9bcf2bdc2ab4
David Higgins <thehig@gmail.com>
2017-11-06 22:51:02 +0000

    Add episode number detection for show name identification

--- autoFilebot.js
+++ autoFilebot.js
@@ -62,15 +62,41 @@ const recurseDirForVideos = (directory, ignorePatterns) =>
       if (err) reject(err);
       const videoFiles = files.filter(isVideo);
       if (c.debug) console.log(`Found ${videoFiles.length} video(s)`);
-      videoFiles.length == 0
-        ? reject("No video files found")
-        : resolve(videoFiles);
+      resolve(videoFiles);
+      // videoFiles.length == 0
+      //   ? reject("No video files found")
+      //   : resolve(videoFiles);
     });
   });
 
-// const getShowName = filename => new Promise((resolve, reject)=>{
-//   console.log(`getShowName: ${filename}`)
-// });
+const getShowName = filename => new Promise((resolve, reject)=>{
+  // Example inputs (folder trimmed)
+  // .../Brooklyn Nine-Nine - 5x05 - Bad Beat.mp4
+  // .../Cleverman - 2x06 - Borrowed Time.mp4
+  // .../DC's Legends of Tomorrow - 3x04 - Phone Home.mkv
+  // .../Last Week Tonight with John Oliver - 4x28 - Episode 117.mkv
+  // .../Scorpion - 4x06 - Queen Scary.mkv
+  // .../The Flash (2014) - 4x04 - Elongated Journey Into Night.mkv
+  // .../Travelers (2016) - 2x03 - Jacob.mkv
+  
+  // Separate at the episode number
+  //  Some numbers, an 'x', some numbers
+  const episodeMatch = /(\d+)?[xXeE](\d+)/.exec(filename);
+  if(!episodeMatch) reject(new Error('Unable to detect show episode ' + filename));
+
+  // Remove the folder (c.temp) and everything from the - before the episode number
+  const showName = filename.substring(c.temp.length + 1, episodeMatch.index - 2).trim();
+  if(c.debug) console.log(`Show Name: ${showName}`);
+
+  // Remove year brackets if present
+  // const yearMatch = /\d+/.exec(showName);
+  // if(!yearMatch) resolve(showName);
+
+  if(c.debug) console.log(`Has Year: ${showName}`);
+  resolve(showName);
+});
+
+const createShowDictionary = filenames => filenames.reduce((prev, next) => { prev[next] = true; return prev; }, {});
 
 checkTempExists(c.temp)
   .then(() => recurseDirForVideos(c.from, c.ignorePatterns))
@@ -81,5 +107,8 @@ checkTempExists(c.temp)
   .then(() => runFilebot(c.temp))
   .then(log => appendToLog(c.temp, log))
   .then(() => recurseDirForVideos(c.temp, c.ignorePatterns))
-  .then(msg => console.log(`Output: ${msg}`))
+  .then(files => Promise.all(files.map(f => getShowName(f))))
+  .then(showNames => createShowDictionary(showNames))
+  .then(msg => console.log(`Output: ${JSON.stringify(msg, null, 4)}`))
+  // .then(msg => console.log(`Output: ${msg}`))
   .catch(err => console.log(`Error: ${err}`));
