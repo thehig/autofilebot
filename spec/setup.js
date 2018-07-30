eeb6961238ee2b4290fef0204755a5c65d1ca1f4
David Higgins <thehig@gmail.com>
2018-07-30 21:45:07 +0100

    Add mock fs

---
+++ spec/setup.js
@@ -0,0 +1,23 @@
+// Create Fixtures
+const mockFs = require('mock-fs');
+const config = require('config');
+
+const fromDir = config.get('from');
+const tempDir = config.get('temp');
+
+mockFs({
+  [fromDir]: {
+    // unprocessed video
+    'RARBG.mp4': '',// ignored
+    incomplete: { // all ignored
+      'someFile.mp4': ''
+    },
+    complete: {
+      'someFile.mp4': '', // included
+      'someFile.sample.mp4': '', // ignored
+      'someFile sample.mp4': '', // ignored
+      'someFile SAMPLE.mp4': '', // ignored
+      'someFile Sample.mp4': '' // ignored
+    }
+  }
+});
