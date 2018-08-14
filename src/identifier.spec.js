83fd384e4917eaa48e3f352a4b4941db64d4e88e
David Higgins <thehig@gmail.com>
2018-08-14 21:18:25 +0100

    Outline testing plan for identifier

--- src/identifier.spec.js
+++ src/identifier.spec.js
@@ -1,37 +1,34 @@
 const id = require("./identifier");
 
-// const source = [
-//   "Elementary - 6x14 - Through the Fog.mkv",
-//   "Elementary - 6x15 - How to Get a Head.mkv",
-//   "Killjoys - 4x04 - What to Expect When You're Expecting... An Alien Parasite.mkv",
-//   "Last Week Tonight with John Oliver - 5x20 - Episode 139.mkv",
-//   "Preacher - 3x08 - The TomBrady.mkv",
-//   "Suits - 8x04 - Revenue Per Square Foot.mkv"
-// ];
+describe('identifier', () => {
+  /*
+    it takes a filepath as a parameter
+      it takes the last element as the file
+      it takes the second last element as the parent
+      it takes the remaining elements as unprocessed
 
-// Edge Case in file.split:
-// [Strange Pilot.mkv] in D:\Sorted Torrents\TV\Archer (2009)\Archer (2009) - 9x01 - Danger Island - Strange Pilot.mkv​​
+    it splits the file name
+      it splits the name into 'Show', 'Ep' and 'Title'
+      it can handle additional split characters without error
+        // [Strange Pilot.mkv] in D:\Sorted Torrents\TV\Archer (2009)\Archer (2009) - 9x01 - Danger Island - Strange Pilot.mkv​​
 
-// describe("identifier", () => {
-//   describe("specific show", () => {
-//     it("Elementary", () => {
-//       expect(id("Elementary - 6x14 - Through the Fog.mkv")).toEqual(
-//         "Elementary"
-//       );
-//     });
-//     it("Killjoys", () => {
-//       expect(id("Killjoys - 6x14 - Through the Fog.mkv")).toEqual("Killjoys");
-//     });
-//     it("Last Week Tonight with John Oliver", () => {
-//       expect(
-//         id("Last Week Tonight with John Oliver - 6x14 - Through the Fog.mkv")
-//       ).toEqual("Last Week Tonight with John Oliver");
-//     });
-//     it("Preacher", () => {
-//       expect(id("Preacher - 6x14 - Through the Fog.mkv")).toEqual("Preacher");
-//     });
-//     it("Suits", () => {
-//       expect(id("Suits - 6x14 - Through the Fog.mkv")).toEqual("Suits");
-//     });
-//   });
-// });
+      
+    it errors if
+      a file name does not match this 'pattern' of `Show - Ep - Title`
+      show, ep or title is empty
+      unprocessed bits contains anything
+
+    it cleans the show name
+      it replaces special characters with a space
+      it replaces special characters with nothing
+      it removes the year (if present)
+
+    it handles special case names
+      it handles Archer (Year needed for Kodi)
+      it handles Doctor Who
+      it handles Planet Earth
+      it handles Last Week Tonight
+      it handles Marvelous Mrs. Maisel
+
+  */
+});
