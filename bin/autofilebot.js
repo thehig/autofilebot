faa1fe3d241c7101bffc03e8828c1b39acc5b9f5
David Higgins <thehig@gmail.com>
2022-05-04 23:48:51 +0100

    Remove unused

--- bin/autofilebot.js
+++ bin/autofilebot.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-// import { usage } from "yargs";
 
 const config = require("config");
 const fromDir = config.get("fromTV");
@@ -8,7 +7,6 @@ const toDir = config.get("to");
 
 const main = require("../src/main").default;
 const wrap = require("../src/consoleWrapper").default;
-// @ts-ignore
 const _main = wrap(main);
 
 _main(fromDir, tempDir, toDir);
